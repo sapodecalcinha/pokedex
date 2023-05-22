68a7d44c93f8ef7c6262557bd0c24ae6f21ebdd0
sapodecalcinha <ennayo@hotmail.com>
2023-05-22 15:19:03 -0300

    feature header update mobile

--- pokedex-vite/src/App.tsx
+++ pokedex-vite/src/App.tsx
@@ -1,10 +1,14 @@
 import { Header } from "./components/Header"
+import { ChakraProvider } from '@chakra-ui/react'
 
 
 function App() {
   return (
     <>
+    <ChakraProvider>
       <Header />
+    </ChakraProvider>
+      
     </>
   )
 }
--- pokedex-vite/src/components/Header/index.tsx
+++ pokedex-vite/src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import styles from './index.module.css'
 import Logo from '../../assets/Logo.svg'
+import Burguer from '../../assets/BurgenBtn.svg'
+import {Menu, MenuButton, MenuList, MenuItem  } from '@chakra-ui/react'
 
 export const Header = () => {
     return (
@@ -10,6 +12,23 @@ export const Header = () => {
                 <a href="#">Pokédex</a>
                 <a href="#">Legendaries</a>
                 <a href="#">Documentation</a>
+            </article>
+
+            <article className={styles.menu}>
+                <Menu>
+                    <MenuButton>
+                        <img src={Burguer} alt="Burguer Button" />
+                    </MenuButton>
+                    <MenuList className={styles.teste}>
+                        <MenuItem className={styles.teste}>
+                            <img src={Logo} alt="Logo Pokemon" />
+                            <a href="#">Home</a>
+                            <a href="#">Pokédex</a>
+                            <a href="#">Legendaries</a>
+                            <a href="#">Documentation</a>
+                        </MenuItem>
+                    </MenuList>
+                </Menu>
             </article>
         </header>
     )
