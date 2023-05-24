50ed96f05b60fb0a531ce7788ac097f0d7c5818b
sapodecalcinha <ennayo@hotmail.com>
2023-05-24 14:04:30 -0300

    alteracoes merge

--- pokedex-vite/src/App.tsx
+++ pokedex-vite/src/App.tsx
@@ -1,17 +1,35 @@
-import { Header } from "./components/Header"
+import styles from './index.module.css'
+import Logo from '../../assets/Logo.svg'
+import Burguer from '../../assets/BurgenBtn.svg'
+import {Menu, MenuButton, MenuList, MenuItem  } from '@chakra-ui/react'
 
-import { Pokedex } from "./components/Pokedex"
+export const Header = () => {
+    return (
+        <header className={styles.header}>
+            <img className={styles.img} src={Logo} alt="Logo Pokémon" />
+            <article className={styles.article}>
+                <a className={styles.a} href="#">Home</a>
+                <a href="#">Pokédex</a>
+                <a href="#">Legendaries</a>
+                <a href="#">Documentation</a>
+            </article>
 
-
-function App() {
-  return (
-    <>
-      <Header />
-      {/*<HomeContent/>*/}
-      <Pokedex/>
-
-    </>
-  )
+            <article className={styles.menu}>
+                <Menu>
+                    <MenuButton>
+                        <img src={Burguer} alt="Burguer Button" />
+                    </MenuButton>
+                    <MenuList className={styles.teste}>
+                        <MenuItem className={styles.teste}>
+                            <img src={Logo} alt="Logo Pokemon" />
+                            <a href="#">Home</a>
+                            <a href="#">Pokédex</a>
+                            <a href="#">Legendaries</a>
+                            <a href="#">Documentation</a>
+                        </MenuItem>
+                    </MenuList>
+                </Menu>
+            </article>
+        </header>
+    )
 }
-
-export default App
