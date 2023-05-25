ea68ce2e01683038fd97ace746390feaf6101d29
sapodecalcinha <ennayo@hotmail.com>
2023-05-25 17:56:36 -0300

    feature pokemon card

---
+++ pokedex-vite/src/components/PokemonCard/index.tsx
@@ -0,0 +1,26 @@
+import styles from './index.module.css'
+import Ellipse from '../../assets/Ellipse 6.svg'
+
+export const PokemonCard = () => {
+    return (
+        <section className={styles.section}>
+            <article className={styles.card_article}>
+                <h1>Quilava</h1>
+                <div className={styles.card_article_props}>
+                    <div className={styles.card_article_direction_column}>
+                        <p>Attack</p>
+                        <div className={styles.card_article_back_num}>
+                            <p>49</p>
+                        </div>
+                    </div>
+                    <div className={styles.card_article_direction_column}>
+                        <p>Defense</p>
+                        <div className={styles.card_article_back_num}>
+                            <p>49</p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </section>
+    )
+}
